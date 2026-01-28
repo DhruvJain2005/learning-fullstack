0f3b3841f66d4eebad5b588f77db45140f998024
DhruvJain2005 <jaindhruv322@gmail.com>
2026-01-28 23:54:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// today we revise our function concepts 
+
+// function declaration 
+// 1  function functionName(){
+// do some work 
+//}
+
+// 2 function functionName(param1,param2){
+// do some work
+//}
+
+
+// function call / invoke same thing
+
+// functionNmae();
+
+// function myfunction(){
+//     console.log("welcome ")
+//     console.log("learning js")
+// }
+
+// myfunction();
+
+
+
+// function hello(name,message){//parameter
+//     console.log(name,message);
+// }
+// hello("dhruv","you are great learner"); // argument
+
+
+// function sumofTwonumbers(num1,num2){// parameters acts as local variable , it has only block scope 
+//     console.log(num1+num2);
+// }
+// sumofTwonumbers(8,5);
+
+
+// ARROW FUNCTION
+
+
+
+// function sum(a,b){      THIS IS NORMAL FUNCTION
+//     return a+b;
+// }
+
+// const arrowsum=(a,b) => {
+//     console.log(a+b);
+// }
+
+// arrowsum(5,2);
+
+// // function mul(a,b){
+// //     return a*b;                   THIS IS NORMAL FUNCTION
+// // }
+
+// const arrowmul=(a,b) => {
+//     console.log(a*b);
+// }
+
+// arrowmul(5,2);
+
+// function countVowels(string){
+//     let count=0;
+//     for(const char of string){
+//         if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u" ){
+//             count ++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+// countVowels("apnacollege");
+
+// const countvowels=(string) => {
+//     let count=0;
+//     for(const char of string){
+//         if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u" ){
+//             count ++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+// countvowels("apnacollege");
+
