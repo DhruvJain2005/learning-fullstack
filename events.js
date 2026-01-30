7a671b47bf1ce5f9dad44ccbf0959dd86f018802
DhruvJain2005 <jaindhruv322@gmail.com>
2026-01-30 23:25:50 +0530

    modified events

--- events.js
+++ events.js
@@ -1,1 +1,32 @@
 console.log("today we learn about events in javascript");
+
+// events in js
+// the change in the state of an object is known as an event 
+// 1. mouse events (click,double click etc.)
+//2. keyboard events(keypress,keyup,keydown)
+//3.form events(submit etc)
+// 4. print event & many more 
+
+// in our html file     //  THIS IS INLINE EVENTS 
+// <button onclick="console.log('button was clicked ');alert('hello')"></button>
+// <button ondblclick="console.log('button was clicked ');alert('hello')"></button>
+//<div onmouseover="console.log('you are inside div')">this is a box </div>
+
+// EVENT HANDLING IS JS , OR DIRECTLY IN JS FILE 
+
+//  let btn1=document.querySelector("#btn1");
+// btn1.onClick=() =>{
+//    console.log("btn1 was created");
+//    
+//    }
+
+// EVENT LISTNERS
+// 1. node.addEventListner(event,callback)
+// 2. node.removeEventListner(event,callback)
+// NOTE* the callback reference should be same to remove 
+
+// EXAMPLE 
+
+// btn1.addEventListner("click", () => {
+//   console.log("button was clicked");    
+//})
