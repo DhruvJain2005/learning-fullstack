87b9bf002e20f38ea766b1071567f7a1a9c824a3
DhruvJain2005 <jaindhruv322@gmail.com>
2026-02-10 16:47:32 +0530

    making netflix clone

---
+++ netflix,html,css,js/script.js
@@ -0,0 +1,10 @@
+// Navbar background on scroll
+window.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 100) {
+        navbar.classList.add("active");
+    } else {
+        navbar.classList.remove("active");
+    }
+});
